c5e3a68393e6482183c721f0130f26075119d425
Cole Bessee <cole.bessee@critigen.com>
2019-08-22 17:53:10 -0700

    add actual data from Matt

--- src/app/shared/images-config.ts
+++ src/app/shared/images-config.ts
@@ -12,16 +12,16 @@ export class ImagesConfig {
     this.images = [
       {
         id: 1,
-        title: 'Test 1',
-        desc: 'This is a description',
+        title: 'Addition of Water Bodies',
+        desc: 'Numerous features have been added to represent ponds near Detroit Lakes, MN.',
         images: [
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug1.jpg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_a/a_2011.png',
             alt: '',
             year: '2011'
           },
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug2.jpeg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_a/a_2019.png',
             alt: '',
             year: '2019'
           }
@@ -29,16 +29,16 @@ export class ImagesConfig {
       },
       {
         id: 2,
-        title: 'Test 2',
-        desc: 'This is a description',
+        title: 'Shifting Riverine Features',
+        desc: 'This feature, representing a stretch of the Minnesota River has been altered to better reflect the true  watercourse.',
         images: [
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug1.jpg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_b/b_2011.png',
             alt: '',
             year: '2011'
           },
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug2.jpeg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_b/b_2019.png',
             alt: '',
             year: '2019'
           }
@@ -46,16 +46,16 @@ export class ImagesConfig {
       },
       {
         id: 3,
-        title: 'Test 3',
-        desc: 'This is a description',
+        title: 'Alteration of Numerous Water Features',
+        desc: 'These features’ geometries have been altered within the Minnesota Valley National Wildlife Refuge.',
         images: [
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug1.jpg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_c/c_2011.png',
             alt: '',
             year: '2011'
           },
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug2.jpeg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_c/c_2019.png',
             alt: '',
             year: '2019'
           }
@@ -63,16 +63,16 @@ export class ImagesConfig {
       },
       {
         id: 4,
-        title: 'Test 4',
-        desc: 'This is a description',
+        title: 'Expansion of Wetlands',
+        desc: 'Wetland features have been added to in this area outside Ortonville, MN.',
         images: [
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug1.jpg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_d/d_2011.png',
             alt: '',
             year: '2011'
           },
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug2.jpeg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_d/d_2019.png',
             alt: '',
             year: '2019'
           }
@@ -80,16 +80,16 @@ export class ImagesConfig {
       },
       {
         id: 5,
-        title: 'Test 5',
-        desc: 'This is a description',
+        title: 'Realignment of Linear Riverine Features',
+        desc: 'Linear riverine features representing the Cottonwood River have been adjusted to aerial imagery near Marshall, MN.',
         images: [
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug1.jpg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_e/e_2011.png',
             alt: '',
             year: '2011'
           },
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug2.jpeg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_e/e_2019.png',
             alt: '',
             year: '2019'
           }
@@ -97,16 +97,16 @@ export class ImagesConfig {
       },
       {
         id: 6,
-        title: 'Test 6',
-        desc: 'This is a description',
+        title: 'Realignment of Linear Riverine Features',
+        desc: 'Linear riverine features have been adjusted to aerial imagery near Currie, MN.',
         images: [
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug1.jpg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_f/f_2011.png',
             alt: '',
             year: '2011'
           },
           {
-            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/pug2.jpeg',
+            url: 'https://mn-water-images.s3-us-west-2.amazonaws.com/comparison_f/f_2019.png',
             alt: '',
             year: '2019'
           }
